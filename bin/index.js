5580f9ba750437e0c3b398125aab836c27da029e
Abramov Arthur <rifttech.arthur@gmail.com>
2020-03-20 19:00:11 +0300

    initial commit :zap:

---
+++ bin/index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const pkg = require("./../package.json");
+const program = require("commander");
+const { initialze } = require("./../lib");
+
+
+
+program.version(pkg.version);
+
+program
+    .command("init <package>")
+    .description("Create a server package")
+    .action((package) => initialze(package))
+
+program.parse(process.argv);
