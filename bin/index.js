e84760c25c9ec595c2ea24f694852769bccdc0a8
Arthur Abramov <rifttech.arthur@gmail.com>
2020-03-22 12:46:15 +0300

    fix - code formatting

--- bin/index.js
+++ bin/index.js
@@ -4,13 +4,11 @@ const pkg = require("./../package.json");
 const program = require("commander");
 const { initialze } = require("./../lib");
 
-
-
 program.version(pkg.version);
 
 program
     .command("init <package>")
     .description("Create a server package")
-    .action((package) => initialze(package))
+    .action((package) => initialze(package));
 
 program.parse(process.argv);
